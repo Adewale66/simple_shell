3dbc460f9ba33fe3caa5e78a173cbf684fa62927
Adewale Kujore <adewalekujore6@gmail.com>
2023-11-15 17:08:13 +0100

    why

--- shell.c
+++ shell.c
@@ -5,28 +5,39 @@
  * @filename: name of the file
  * Return: EXIT_STATUS
  */
-
 int shell(char *filename)
 {
 	char *line;
 	int stat_code = 0;
 	int multiple = -1;
 
-	while (1)
+	if (!isatty(STDIN_FILENO))
 	{
-		if (isatty(STDIN_FILENO))
+		while ((line = read_line()))
+		{
+			multiple = check_for_multiple(line);
+			if (multiple > 0)
+				stat_code = handle_multiple(line, multiple, filename, stat_code);
+			else if (multiple == 0)
+				stat_code = 0;
+			else
+				stat_code = parse_line(line, filename, stat_code);
+		}
+		return (stat_code);
+	}
+	else
+	{
+		while (1)
+		{
 			write(STDIN_FILENO, "$ ", 2);
-		line = read_line();
-		if (line == NULL && isatty(STDIN_FILENO))
-			continue;
-		else if (line == NULL && !isatty(STDIN_FILENO))
-			return (stat_code);
-		multiple = check_for_multiple(line);
-		if (multiple > 0)
-			stat_code = handle_multiple(line, multiple, filename, stat_code);
-		else if (multiple == 0)
-			stat_code = 0;
-		else
-			stat_code = parse_line(line, filename, stat_code);
+			line = read_line();
+			multiple = check_for_multiple(line);
+			if (multiple > 0)
+				stat_code = handle_multiple(line, multiple, filename, stat_code);
+			else if (multiple == 0)
+				stat_code = 0;
+			else
+				stat_code = parse_line(line, filename, stat_code);
+		}
 	}
 }
